9ef3224c510eede0ee20143afaf9a95e55e0793d
Souravgguptan <souravgupta3331@gmail.com>
2024-04-30 18:08:54 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const state = {
+    taskList: [
+        {
+            title: "",
+            url: "",
+            type: "",
+            descripion: ""
+        },
+         {
+            title: "",
+            url: "",
+            type: "",
+            descripion: ""
+        }, 
+        {
+            title: "",
+            url: "",
+            type: "",
+            descripion: ""
+        }
+    ]
+}
+
+
+// DOM:
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+// console.log(taskModal)
